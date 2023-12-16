0dd9071c237da781dd0098ff3098ae5755c1413b
DK8851 <131203570+DK8851@users.noreply.github.com>
2023-12-15 21:25:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function hireMe(){
+    location.replace("https://stuconestogacon-my.sharepoint.com/:b:/g/personal/dkalathiya4644_conestogac_on_ca/ERLZT2bmrWtGt7Pb-8CFUKABey-tWXU2MBRqXHWjjPdzyA?e=dbRTmv")
+}
